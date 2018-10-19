6b5b3c6742e3684887c07325fe4741eec2ee8080
GeorgeMarkham <george@gmarkham.com>
2018-10-19 12:21:40 +0100

    Created Controller class with main method

---
+++ Interpreter/Controller.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Interpreter{
+    class Controller{
+        static void Main(string[] args){
+        }
+    }
+}
