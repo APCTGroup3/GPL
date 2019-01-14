083bacca4f52aa59ac09ecb2f245322c799e5357
Liam Self <liam@liamself.co.uk>
2019-01-14 02:08:22 +0000

    Revert "Merge Arrays branch"
    This reverts commit e051cfea214f58e6dfe2345abd062048af042380.

---
+++ Interpreter/Interpreter/CoreParser/Parser/AST/ArrayElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreParser.Parser.AST
+{
+    public class ArrayElement : Node
+    {
+        public Node Element { get; set; }
+        public ArrayElement(Token token, Node element) : base(token) 
+        {
+            Element = element;
+        }
+
+        public override List<Node> Children()
+        {
+            return new List<Node>();
+        }
+    }
+}
